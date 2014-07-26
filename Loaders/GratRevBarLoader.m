12651b9132aaf1a04d71d46b609ec4a020654f3c
Brandon Jackson <brandonscottjackson@gmail.com>
2014-07-27 01:50:10 +0200

    random gratrev stimuli generated using correct stim size

--- Loaders/GratRevBarLoader.m
+++ Loaders/GratRevBarLoader.m
@@ -24,7 +24,7 @@ classdef GratRevBarLoader < SuperLoader
             
             stimulusParams = {};
             stimulusParams.rmult = 1;
-            stimulusParams.stimulusSize = 164;
+            stimulusParams.stimulusSize = GratRevUtil.getStimulusSize(GRL.pf);
             stimulusParams.ori = oris(randi(length(oris)));
             stimulusParams.phase = phases(randi(length(phases)));
             stimulusParams.sf = sfs(randi(length(sfs)));
