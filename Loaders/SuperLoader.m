704044a208795abc70d2b52e6a21d43ad12e3f44
Brandon Jackson <brandonscottjackson@gmail.com>
2014-07-27 14:37:34 +0200

    SuperLoader.plotRandomStimuli() added

--- Loaders/SuperLoader.m
+++ Loaders/SuperLoader.m
@@ -22,6 +22,21 @@ classdef SuperLoader < handle
         % RANDOMSTIMULUS loads a random image into memory
         %   RETURN img                  (double matrix) grayscale image
         end
+        
+        function [] = plotRandomStimuli(SL)
+        % PLOTRANDOMSTIMULI displays a grid of 12 random stimuli
+            figure();
+            for i=1:12
+                subplot(3,4,i);
+                stim = SL.randomStimulus();
+                imshow(stim);
+                axis square;
+            end
+            boxtitle(sprintf('%s Random Stimuli (%d x %d px)',...
+                PFUtil.experName(SL.pf),...
+                size(stim,1),...
+                size(stim,2)));
+        end
 
     end
 end
